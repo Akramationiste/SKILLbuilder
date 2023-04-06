d177db70ba813c81c97ff056e31889dfe9e442cb
unknown <hamai.ismail.dz@gmail.com>
2023-04-06 11:08:14 +0200

    v0.7

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from './components/navbar/Navbar.jsx'
 import Stat from './components/Stat'
+import Carousel from './components/carousel/Carousel.jsx';
 
 
 function App() {
   return (
     <div className="App">
     <Navbar/>
+    <Carousel/>
     <Stat/>
     </div>
       
